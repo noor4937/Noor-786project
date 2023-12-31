c3399c0300d3b46924c12e3b30d2d79bd628f1d5
Noorjahan <noor4937@gmail.com>
2023-12-31 19:17:19 +0000

    to the branch

--- src/sdet1_class.java
+++ src/sdet1_class.java
@@ -4,6 +4,8 @@ public class sdet1_class {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
    System.out.println("committing from sde1");
+   //adding as part of branching
+   System.out.println("commt for branch");
    }
 
 }
